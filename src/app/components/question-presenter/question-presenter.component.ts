da63f23e5ddd5af941075ff70c4853aa8521dc08
Paolo Paci <paolopci@yahoo.it>
2026-06-08 17:01:52 +0200

    refactor: update QuestionPresenterComponent to use QuizStore for currentQuestion

--- src/app/components/question-presenter/question-presenter.component.ts
+++ src/app/components/question-presenter/question-presenter.component.ts
@@ -1,18 +1,27 @@
-import { Component, EventEmitter, Input, Output, signal } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  inject,
+  Input,
+  Output,
+  signal,
+} from '@angular/core';
 import { Question } from '../../models/question.model';
 import { SharedModule } from '../../shared.module';
 import { FormControl, Validators } from '@angular/forms';
+import { QuizStore } from '../../store/quiz.store';
 
 @Component({
-    selector: 'app-question-presenter',
-    imports: [SharedModule],
-    templateUrl: './question-presenter.component.html',
-    styleUrl: './question-presenter.component.scss'
+  selector: 'app-question-presenter',
+  imports: [SharedModule],
+  templateUrl: './question-presenter.component.html',
+  styleUrl: './question-presenter.component.scss',
 })
 export class QuestionPresenterComponent {
-  readonly question = signal<Question>({
-    caption: ['Red', 'Green'],
-    answers: ['Red', 'Green', 'Blue', 'Yellow'],
-    correctIndex: 3
-  });
+  readonly store = inject(QuizStore);
+
+  // Perché qui non stai leggendo il valore del signal: stai copiando il riferimento al signal.
+  // Quindi question diventa semplicemente un alias locale del signal.
+  // Per questo non usi () in TypeScript.
+  readonly question = this.store.currentQuestion;
 }
