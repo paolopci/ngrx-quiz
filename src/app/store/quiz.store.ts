fda64d695e04c9d1393b35f97d51c31cba1a9d80
Paolo Paci <paolopci@yahoo.it>
2026-06-08 17:16:23 +0200

    refactor: restructure withComputed for clarity in QuizStore

--- src/app/store/quiz.store.ts
+++ src/app/store/quiz.store.ts
@@ -5,11 +5,16 @@ import { computed } from '@angular/core';
 export const QuizStore = signalStore(
   { providedIn: 'root' },
   withState(initialQuizSlice),
-  withComputed((p) => ({
-    currentQuestionIndex: computed(() => p.answers().length), // conta il numero delle risposte che ho già dato
-    // se il numero delle risposte date è uguale al numero delle domande allora ho finito il quiz
-    isDone: computed(() => p.answers().length === p.questions().length),
-  })),
+  withComputed((p) => {
+    const currentQuestionIndex = computed(() => p.answers().length); // conta il numero delle risposte che ho già dato
+    const isDone = computed(() => p.answers().length === p.questions().length);
+    return {
+      currentQuestionIndex,
+      // se il numero delle risposte date è uguale al numero delle domande allora ho finito il quiz
+      isDone,
+    };
+  }),
+
   withComputed((p) => ({
     // 1° metodo x ritornare la domanda corrente ma ... migliorabile
     currentQuestion: computed(() => p.questions()[p.currentQuestionIndex()]),
