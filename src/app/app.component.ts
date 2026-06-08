99ed69379099f9b2e8dd0717e0d5d23e5203e40d
Paolo Paci <paolopci@yahoo.it>
2026-06-08 16:19:19 +0200

    refactor: update app component to use QuizStore for state management

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,20 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { SharedModule } from './shared.module';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { ProgressComponent } from './components/progress/progress.component';
 import { DoneComponent } from './components/done/done.component';
 import { BusyComponent } from './components/busy/busy.component';
-import { QUESTIONS } from './data/questions';
-
+import { QuizStore } from './store/quiz.store';
 
 @Component({
   selector: 'app-root',
   imports: [SharedModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  providers: [QuizStore],
 })
 export class AppComponent {
   title = 'ngrx-quiz';
-  questions = signal(QUESTIONS);
+
+  readonly store = inject(QuizStore);
 }
