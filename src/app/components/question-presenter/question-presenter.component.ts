0cb1595069e5d23685123d51c7c513042248c468
Paolo Paci <paolopci@yahoo.it>
2026-06-09 10:56:55 +0200

    refactor: enhance QuestionPresenterComponent to update answers in QuizStore on selection

--- src/app/components/question-presenter/question-presenter.component.ts
+++ src/app/components/question-presenter/question-presenter.component.ts
@@ -10,6 +10,7 @@ import { Question } from '../../models/question.model';
 import { SharedModule } from '../../shared.module';
 import { FormControl, Validators } from '@angular/forms';
 import { QuizStore } from '../../store/quiz.store';
+import { patchState } from '@ngrx/signals';
 
 @Component({
   selector: 'app-question-presenter',
@@ -24,4 +25,13 @@ export class QuestionPresenterComponent {
   // Quindi question diventa semplicemente un alias locale del signal.
   // Per questo non usi () in TypeScript.
   readonly question = this.store.currentQuestion;
+
+  onSelect(index: number) {
+    console.log('Selecte answer: ', index);
+
+    patchState(this.store,state=>({
+      answers: [...state.answers,index]
+    
+    }))
+  }
 }
--- src/app/store/quiz.store.ts
+++ src/app/store/quiz.store.ts
@@ -3,20 +3,29 @@ import { initialQuizSlice } from './quiz.slice';
 import { computed } from '@angular/core';
 
 export const QuizStore = signalStore(
-  { providedIn: 'root' },
+  {
+    providedIn: 'root',
+    //protectedState: false, // questo ti permette di modificare lo stato direttamente It looks like you're working with Angular's signal
+  },
   withState(initialQuizSlice),
   withComputed((p) => {
     const currentQuestionIndex = computed(() => p.answers().length); // conta il numero delle risposte che ho già dato
+    // se il numero delle risposte date è uguale al numero delle domande allora ho finito il quiz
     const isDone = computed(() => p.answers().length === p.questions().length);
+    // per currentQuestion occorre che currentQuestionIndex sia già stato calcolato
+    // quindi importante è l'ordine
+    const currentQuestion = computed(
+      () => p.questions()[currentQuestionIndex()],
+    );
     return {
       currentQuestionIndex,
-      // se il numero delle risposte date è uguale al numero delle domande allora ho finito il quiz
       isDone,
+      currentQuestion,
     };
   }),
 
-  withComputed((p) => ({
-    // 1° metodo x ritornare la domanda corrente ma ... migliorabile
-    currentQuestion: computed(() => p.questions()[p.currentQuestionIndex()]),
-  })),
+  // withComputed((p) => ({
+  //   1° metodo x ritornare la domanda corrente ma ... migliorabile
+  //   currentQuestion: computed(() => p.questions()[p.currentQuestionIndex()]),
+  // })),
 );
